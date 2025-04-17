8012c9503be590ea6158cf8d1eb549d1935e3ca9
Suryapratapsinh Thakor <108937823+suryapratapsinh@users.noreply.github.com>
2025-04-17 13:12:35 +0530

    Update database.py

--- database.py
+++ database.py
@@ -1,18 +1,28 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "mysql+pymysql://root:@localhost/fastapi_user"
+DATABASE_URL = os.getenv("DATABASE_URL")  # pulls from ENV vars
+# DATABASE_URL = "mysql+pymysql://root:@localhost/fastapi_user"
 
 # Connect to DB
 engine = create_engine(DATABASE_URL)
 # engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 
 # Create a session factory to talk to DB
-SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
+# SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
 # SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
+
 
 # Base class for models
 Base = declarative_base()
+
+
+DATABASE_URL = os.getenv("DATABASE_URL")  # pulls from ENV vars
+
+
+
 
 
 
