bb70553b22eeb1462edcf9b305a81296a28f6414
Suryapratapsinh Thakor <108937823+suryapratapsinh@users.noreply.github.com>
2025-04-18 16:57:12 +0530

    Update database.py

--- database.py
+++ database.py
@@ -1,25 +1,35 @@
-import os
+# import os
+# from sqlalchemy import create_engine
+# from sqlalchemy.orm import sessionmaker, declarative_base
+
+# DATABASE_URL = os.getenv("DATABASE_URL")  # pulls from ENV vars
+# # DATABASE_URL = "mysql+pymysql://root:@localhost/fastapi_user"
+
+# # Connect to DB
+# engine = create_engine(DATABASE_URL)
+# # engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+
+# # Create a session factory to talk to DB
+# # SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
+# # SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+# SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
+
+
+# # Base class for models
+# Base = declarative_base()
+
+
+# DATABASE_URL = os.getenv("DATABASE_URL")  # pulls from ENV vars
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = os.getenv("DATABASE_URL")  # pulls from ENV vars
-# DATABASE_URL = "mysql+pymysql://root:@localhost/fastapi_user"
+# Use your actual URL here
+DATABASE_URL = "postgresql://postgres:Password_30112002@db.bqghunzqudzmsmnvnsvh.supabase.co:5432/postgres" 
 
-# Connect to DB
 engine = create_engine(DATABASE_URL)
-# engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
-
-# Create a session factory to talk to DB
-# SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
-# SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
-
-
-# Base class for models
 Base = declarative_base()
-
-
-DATABASE_URL = os.getenv("DATABASE_URL")  # pulls from ENV vars
 
 
 
