ba7b6dea7a1a8d68d581d4aca430d0fb1c102c24
Suryapratapsinh Thakor <108937823+suryapratapsinh@users.noreply.github.com>
2025-05-02 17:16:32 +0530

    Update models.py

--- models.py
+++ models.py
@@ -1,14 +1,30 @@
 from sqlalchemy import Column, Integer, String, Float
 from database import Base
 
+# class User(Base):
+#     __tablename__ = "users"
+
+#     id = Column(Integer, primary_key=True, index=True)
+#     name = Column(String(100))
+#     age = Column(Integer)
+#     contact = Column(String(20))
+#     email = Column(String(100))
+#     pr_10 = Column(Float)
+#     pr_12 = Column(Float)
+#     graduation_gpa = Column(Float)
+
+
+
 class User(Base):
-    __tablename__ = "users"
+    __tablename__ = "users_info_table"
 
     id = Column(Integer, primary_key=True, index=True)
-    name = Column(String(100))
-    age = Column(Integer)
-    contact = Column(String(20))
-    email = Column(String(100))
-    pr_10 = Column(Float)
-    pr_12 = Column(Float)
-    graduation_gpa = Column(Float)
+    Full_Name = Column(String(100))
+    Email = Column(String(100))
+    Phone = Column(String(20))
+    Country_of_Interest = Column(String(20))
+    Preferred_Intake = Column(String(20))
+    Highest_Qualification = Column(String(30))
+    IELTS_TOEFL_Score = Column(Float)
+    Message = Column(String(200))
+
